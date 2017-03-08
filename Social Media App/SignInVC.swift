6b4b0e9dc53f15eec8f4acfc6e8f4eb711f3f4ad
Saska Rauhala <saskarauhala@Saska-MBP.home>
2017-03-08 21:13:12 +0200

    Firebase setup

---
+++ Social Media App/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Media App
+//
+//  Created by Saska Rauhala on 8.3.2017.
+//  Copyright © 2017 SarTekh. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
